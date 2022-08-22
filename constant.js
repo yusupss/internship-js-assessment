efd9940555419c081abdcb7821c2aecbbfb1cff7
ErickEzrandy274 <erick.ezrandy@ui.ac.id>
2022-08-22 15:27:29 +0700

    add main functionality

---
+++ constant.js
@@ -0,0 +1,28 @@
+const defaultData = [
+  {
+    name: 'Alfa',
+    score: 5,
+  },
+  {
+    name: 'Beta',
+    score: 4,
+  },
+  {
+    name: 'Charlie',
+    score: 3,
+  },
+  {
+    name: 'Gamma',
+    score: 2,
+  },
+  {
+    name: 'Theta',
+    score: 1,
+  },
+];
+
+let timer = 20;
+let currentScore = 0;
+let firstOperand, secondOperand, operator, result;
+let index;
+let higher;
--- script.js
+++ script.js
@@ -1,24 +1,119 @@
 'use strict';
 
-let highscore = [];
-let timer = 20;
+let highscore = [...defaultData];
 const timerElement = document.querySelector('.timer');
 const buttonPlay = document.querySelector('.btn-play');
 const playGameSection = document.querySelector('.play-game');
 const questionMarkEl = document.querySelector('#questionMark');
 const questionFieldEl = document.querySelector('#questionField');
+const finalScoreEl = document.querySelector('#finalScore');
+const finalScoreParentEl = document.querySelector('.finalScoreParent');
+const firstOperandEl = document.querySelector('#first--operand');
+const secondOperandEl = document.querySelector('#second--operand');
+const operatorEl = document.querySelector('#operator');
+const checkButton = document.querySelector('.check');
+const answerEl = document.querySelector('.answer');
+const messageEl = document.querySelector('.message');
+const scoreEl = document.querySelector('.score');
+const formHighScoreEl = document.querySelector('#form-highscore');
+const highscoreUserEl = document.querySelector('#highscoreUser');
+const submitBtnEl = document.querySelector('#submit-btn');
+const DEFAULT_OPERATOR = ['+', '-', 'x'];
+
+const generateOperand = () => {
+  return Math.trunc(Math.random() * 9 + 1);
+};
+
+const generateOperatorIndex = () => {
+  return Math.trunc(Math.random() * 3);
+};
+
+const generateResult = (firstOperand, secondOperand, operator) => {
+  if (operator === '+') return firstOperand + secondOperand;
+  else if (operator === '-') return firstOperand - secondOperand;
+  else if (operator === 'x') return firstOperand * secondOperand;
+};
+
+const generateQuestion = () => {
+  firstOperand = generateOperand();
+  secondOperand = generateOperand();
+  operator = DEFAULT_OPERATOR[generateOperatorIndex()];
+
+  firstOperandEl.textContent = firstOperand;
+  secondOperandEl.textContent = secondOperand;
+  operatorEl.textContent = operator;
+
+  result = generateResult(firstOperand, secondOperand, operator);
+};
+
+const endGame = () => {
+  questionFieldEl.classList.add('hidden');
+  finalScoreParentEl.classList.remove('hidden');
+  finalScoreEl.textContent = currentScore;
+  messageEl.textContent = '💥 Times up!!!';
+
+  index = highscore.findIndex(({ score }) => score === currentScore);
+  higher = index === -1 && currentScore > highscore[0].score;
+  ((index !== -1 && index !== 4) || higher) &&
+    formHighScoreEl.classList.toggle('hidden');
+};
 
 const playGame = () => {
-  questionMarkEl.classList.add('hidden');
-  questionFieldEl.classList.remove('hidden');
-  buttonPlay.remove(); // TODO: ketika user klik again add element buttonPlay!
-  playGameSection.classList.remove('hidden');
+  questionMarkEl.classList.toggle('hidden');
+  questionFieldEl.classList.toggle('hidden');
+  buttonPlay.classList.toggle('hidden');
+  playGameSection.classList.toggle('hidden');
 
   let interval = setInterval(() => {
     timer--;
     timerElement.textContent = timer;
-    timer === 0 && clearInterval(interval);
+    if (timer === 0) {
+      clearInterval(interval);
+      endGame();
+    }
   }, 1000);
+
+  generateQuestion();
+};
+
+const checkingAnswer = () => {
+  if (timer) {
+    const answer = +answerEl.value;
+    messageEl.textContent =
+      answer === result
+        ? '🎉 Correct Answer! Answer next!'
+        : '🚫 Wrong Answer! Answer next!';
+
+    scoreEl.textContent = answer === result ? ++currentScore : currentScore;
+
+    generateQuestion();
+    answerEl.value = '';
+  }
+};
+
+const checkingUsername = e => {
+  console.log(e.key);
+  e.preventDefault();
+  const name = highscoreUserEl.value;
+
+  if ((index !== -1 && index !== 4) || higher) {
+    const newUser = { name, score: currentScore };
+    highscore.splice(index + 1, 0, newUser);
+    highscore.pop();
+
+    for (const [index, { name, score }] of highscore.entries()) {
+      document.querySelector(`#best-player-${index}`).textContent = name;
+      document.querySelector(`#best-highscore-${index}`).textContent = score;
+    }
+  }
+
+  highscoreUserEl.value = '';
+  formHighScoreEl.classList.toggle('hidden');
 };
 
 buttonPlay.addEventListener('click', playGame);
+checkButton.addEventListener('click', checkingAnswer);
+formHighScoreEl.addEventListener('submit', checkingUsername);
+document.addEventListener('keydown', e => {
+  e.key === 'Enter' && +answerEl.value && checkingAnswer();
+});
