b8b45a7e33fc39e3c39986b361e803859ba10b1c
Julian_A_Wardana <julian.wardana@binus.ac.id>
2022-08-22 10:41:47 +0700

    feat: making template for html & js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const scoreBoard = [
+    {name: "Julian", point: 5},
+    {name: "Alvian", point: 4},
+    {name: "Fajar", point: 3},
+    {name: "Rahmat", point: 2},
+    {name: "Leo", point: 1},
+]
+
+document.getElementById('score-board').innerHTML = scoreBoard.map((score, rank) =>
+    `<p class="score-board">${rank + 1}. ${score.name} <span class="score">${score.point}</span></p>`
+).join('<br>');
