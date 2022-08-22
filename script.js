3a5fc54bac4a9a02b6dd33b3050e294066913de6
Julian_A_Wardana <julian.wardana@binus.ac.id>
2022-08-22 10:55:21 +0700

    feat: add timer

--- script.js
+++ script.js
@@ -6,8 +6,29 @@ const scoreBoard = [
     {name: "Fajar", point: 3},
     {name: "Rahmat", point: 2},
     {name: "Leo", point: 1},
-]
+];
+
+let timeLeft = 20;
+
+
+document.addEventListener("DOMContentLoaded", function(event) { 
+    let countdown = setInterval(function(){
+        timeLeft--;
+
+    document.getElementById("time").textContent = timeLeft;
+    if(timeLeft <= 0)
+        clearInterval(countdown);
+    }, 1000);
+});
 
 document.getElementById('score-board').innerHTML = scoreBoard.map((score, rank) =>
-    `<p class="score-board">${rank + 1}. ${score.name} <span class="score">${score.point}</span></p>`
+    `<p class="score-board">${rank + 1}. ${score.name}... <span class="score">${score.point}</span></p>`
 ).join('<br>');
+
+function ready(callback){
+    if (document.readyState!='loading') callback();
+    else if (document.addEventListener) document.addEventListener('DOMContentLoaded', callback);
+    else document.attachEvent('onreadystatechange', function(){
+        if (document.readyState=='complete') callback();
+    });
+}
