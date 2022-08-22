246cc8baf6cc7d8406905f18bc012357906e264f
Julian_A_Wardana <julian.wardana@binus.ac.id>
2022-08-22 18:18:20 +0700

    feat: finish adding name and score to scoreboard

--- script.js
+++ script.js
@@ -1,43 +1,58 @@
 'use strict';
 
-const scoreBoard = [
-    {name: "Julian", point: 5},
-    {name: "Alvian", point: 4},
-    {name: "Fajar", point: 3},
-    {name: "Rahmat", point: 2},
-    {name: "Leo", point: 1},
-];
+const scoreBoard = new Map([
+    ["Julian", 5],
+    ["Alvian", 4],
+    ["Fajar", 3],
+    ["Rahmat", 2],
+    ["Leo", 1],
+]);
 
-let timeLeft = 20;
+let timeLeft;
 let result = 0;
 let score = 0;
+let rank = 1;
+let highscore = '';
+let sortedDesc = new Map();
+let inputNumber = document.querySelector('.guess');
+let timer = document.getElementById("time");
 
 const content = document.querySelector(".content");
-const inputNumber = document.querySelector('.guess');
 const buttonCheck = document.querySelector('.check');
 const message = document.querySelector('.message');
+const inputName = document.querySelector('.inputName');
+const user  = document.querySelector('.username');
+const buttonSubmit = document.querySelector('.submit');
+
+scoreBoard.forEach((value, key) => {
+    highscore += `<p class="score-board">${rank}. ${key}... <span class="score">${value}</span></p><br>`;
+    rank++;
+});
+
+document.getElementById('score-board').innerHTML = highscore;
 
 function getRandomInt (min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
 function compute() {
-    const operators = ["+", "-", "*", "÷"];
-    content.innerHTML = (Math.trunc(Math.random() * 10) + 1) + operators[getRandomInt(0,3)] + (Math.trunc(Math.random() * 10) + 1);
+    const operators = ["+", "-", "*"];
+    content.innerHTML = (Math.trunc(Math.random() * 10) + 1) + operators[getRandomInt(0,2)] + (Math.trunc(Math.random() * 10) + 1);
     result = eval(document.querySelector(".content").textContent);
 
     return result;
 };
 
-document.addEventListener("DOMContentLoaded", function(event) {
-    compute();
+function play() {
+    timeLeft = 20;
     let countdown = setInterval(function() {
         timeLeft--;
+        timer.textContent = timeLeft;
 
         inputNumber.addEventListener('keyup', e => {
             e.preventDefault();
             if (e.keyCode === 13) {
-                button.click();
+                buttonCheck.click();
             };
         });
 
@@ -47,12 +62,11 @@ document.addEventListener("DOMContentLoaded", function(event) {
             if (guess == result) {
                 score++;
                 document.querySelector('.score').textContent = score;
-                document.querySelector('.guess').value = '';
+                inputNumber.value = '';
                 compute();
             };
         });
 
-        document.getElementById("time").textContent = timeLeft;
         if (timeLeft <= 0) {
             clearInterval(countdown);
         };
@@ -60,10 +74,42 @@ document.addEventListener("DOMContentLoaded", function(event) {
             message.textContent = '💥Times up!!!';
             content.innerHTML = `Your Score is ${score}`;
             content.style.width = "100rem";
+            inputNumber.value = '';
+            inputName.style.display = 'block';
         };
     }, 1000);
+}
+
+compute();
+play();
+
+document.querySelector('.again').addEventListener('click', function () {
+    timer.textContent = 20;
+    content.textContent = '?';
+    inputNumber.value = '';
+    content.style.width = '30rem';
+    inputName.style.display = 'none';
+
+    document.querySelector('.score') = 0;
+    document.querySelector('body').style.backgroundColor = '#222';
+    compute();
+    play();
 });
 
-document.getElementById('score-board').innerHTML = scoreBoard.map((score, rank) =>
-    `<p class="score-board">${rank + 1}. ${score.name}... <span class="score">${score.point}</span></p>`
-).join('<br>');
+buttonSubmit.addEventListener('click', () => {
+    scoreBoard.set(user.value, score);
+    user.value = '';
+    highscore = '';
+    rank = 1;
+
+    sortedDesc = [...scoreBoard].sort((a, b) => (a[1] > b[1] ? -1 : 1));
+
+    sortedDesc.forEach((value, key) => {
+        if (rank > 5) {
+            return;
+        };
+        highscore += `<p class="score-board">${rank}. ${value[0]}... <span class="score">${value[1]}</span></p><br>`;
+        rank++;
+    });
+    document.getElementById('score-board').innerHTML = highscore;
+});
