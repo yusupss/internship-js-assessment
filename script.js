b80c665d5ae3c68033dae17413417b560ed9482a
jerensl <54782057+jerensl@users.noreply.github.com>
2022-08-23 12:29:57 +0800

    fix: fix duplicating

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ const operation = new Map([
   [2, 'x'],
 ]);
 
+let count = 20;
 let score = 0;
 let operator = operation.get(getRandomInt(0, 2));
 let numOne = getRandomInt(0, 9);
@@ -37,9 +38,7 @@ function calc(numOne, numTwo, operation) {
   }
 }
 
-let count = 20;
-
-const topScores = [
+let topScores = [
   { name: 'Luffy', score: 5 },
   { name: 'Sanji', score: 4 },
   { name: 'Zoro', score: 3 },
@@ -58,7 +57,7 @@ const intervalID = setInterval(() => {
   document.querySelector('.time').textContent = count;
   if (count < 1) {
     if (score > topScores[topScores.length - 1].score) {
-      document.querySelector('.finish').style.display = 'flex';
+      document.querySelector('#new--highscore').style.display = 'flex';
       document.querySelector('.message').textContent = '☄️ Times up !!!';
       document.querySelector('.number').textContent = `Your Score is ${score}`;
     }
@@ -67,7 +66,8 @@ const intervalID = setInterval(() => {
   }
 }, 1000);
 
-document.querySelector('.check').addEventListener('click', () => {
+document.getElementById('check--calculation').addEventListener('submit', e => {
+  e.preventDefault();
   const guessNumber = Number(document.querySelector('.guess').value);
   const totalCalc = calc(numOne, numTwo, operator);
 
@@ -91,15 +91,18 @@ document.querySelector('.check').addEventListener('click', () => {
   }
 });
 
-document.querySelector('.highscore').addEventListener('click', () => {
+document.querySelector('#new--highscore').addEventListener('submit', e => {
+  e.preventDefault();
   const newPlayer = document.querySelector('.name').value;
   console.log(topScores);
   if (
     topScores.some(e => e.name === newPlayer) &&
-    topScores.some(e => e.score <= score)
+    topScores.some(e => e.score >= score)
   ) {
     return;
   }
+
+  topScores = topScores.filter(({ name }) => name !== newPlayer);
 
   if (isUpdated) {
     return;
@@ -120,12 +123,12 @@ document.querySelector('.highscore').addEventListener('click', () => {
   }
 });
 
-document.querySelector('.again').addEventListener('click', () => {
+document.querySelector('.new--game').addEventListener('click', () => {
   score = 0;
   count = 20;
 
   isUpdated = false;
-  document.querySelector('.finish').style.display = 'none';
+  document.querySelector('#new--highscore').style.display = 'none';
   document.querySelector('.message').textContent = 'Start guessing...';
   document.querySelector('.number').textContent = `Your Score is ${score}`;
 
@@ -143,7 +146,7 @@ document.querySelector('.again').addEventListener('click', () => {
     document.querySelector('.time').textContent = count;
     if (count < 1) {
       if (score > topScores[topScores.length - 1].score) {
-        document.querySelector('.finish').style.display = 'flex';
+        document.querySelector('#new--highscore').style.display = 'flex';
         document.querySelector('.message').textContent = '☄️ Times up !!!';
         document.querySelector(
           '.number'
